1d98cef26979f61efff29bc6d4f9f631351c2af1
JLGGG <blockboy2957@gmail.com>
2022-04-05 22:06:55 +0900

    refactor: Do run refactoring on blockchain structure
    This refactoring uses the singleton pattern.

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/JLGGG/TestCoin/blockchain"
+import (
+	"fmt"
+	"github.com/JLGGG/TestCoin/blockchain"
+)
 
 func main() {
 	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
+	}
 }
