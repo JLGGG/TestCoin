32d8cdfd6ef92f8b87454a780c27d9cd33106518
JLGGG <blockboy2957@gmail.com>
2022-05-19 18:17:49 +0900

    feat: rendering templates

--- main.go
+++ main.go
@@ -3,15 +3,26 @@ package main
 import (
 	"fmt"
 	"github.com/JLGGG/TestCoin/blockchain"
+	"html/template"
+	"log"
+	"net/http"
 )
 
+const port string = ":4000"
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	tmpl.Execute(rw, data)
+}
+
 func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
-	}
+	http.HandleFunc("/", home)
+	fmt.Println("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
